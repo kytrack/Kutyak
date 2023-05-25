38cede76382090290553d99710751f40469a0e5d
kytrack <vkrisztianmgy@gmail.com>
2023-05-25 11:47:05 +0200

    4oravege

---
+++ Kutyak/KutyaClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutyak
+{
+    class KutyaClass
+    {
+        int kutyaid;
+        string kutyanev;
+
+        public KutyaClass(int kutyaid, string kutyanev)
+        {
+            this.kutyaid = kutyaid;
+            this.kutyanev = kutyanev;
+        }
+
+        public int Kutyaid { get => kutyaid;}
+        public string Kutyanev { get => kutyanev;}
+    }
+}
--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -1,2 +1,23 @@
-﻿    // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Kutyak;
+
+List<KutyaClass> kutyak = new List<KutyaClass>();
+
+StreamReader sr = new StreamReader("KutyaNevek.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    string[] adatok = sr.ReadLine().Split(";");
+    kutyak.Add(new KutyaClass(int.Parse(adatok[0]), adatok[1]));
+}
+sr.Close();
+
+StreamReader sr2 = new StreamReader("KutyaNevek.csv");
+sr2.ReadLine();
+while (!sr.EndOfStream)
+{
+    string[] adatok = sr2.ReadLine().Split(";");
+    kutyak.Add(new KutyaClass(int.Parse(adatok[0]), adatok[1]));
+}
+sr2.Close();
+
+Console.WriteLine($"3.feladat: Kutyanevek száma: {kutyak.Count()}");
